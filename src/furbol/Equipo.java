4ce4cdf6fb8cea86d963e7f8ab5714e7d5a52aa5
Alex Perez <Alex Perez@DESKTOP-B1TCN21>
2021-03-18 17:25:16 +0100

    "Funciona" pero a medias, falta que funcione bien el tema de empates, goalverage y inicila letra

--- src/furbol/Equipo.java
+++ src/furbol/Equipo.java
@@ -5,41 +5,45 @@
  */
 package furbol;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Alex Perez
  */
-public class Equipo {
-    
+public class Equipo implements Comparable<Equipo> {
+
     private String nombre;
     private int golesFavor;
     private int golesContra;
+    private int golaverage;
     private int win;
     private int draw;
     private int lost;
-    private int puntuacion = win*3 + draw;
+    private int puntuacion = win * 3 + draw;
     private int partidos = win + draw + lost;
 
-    public Equipo(){
+    public Equipo() {
     }
-    
-    public Equipo(String nombre, int golesFavor, int golesContra, int partidos, int puntuacion, int win, int draw, int lost) {
+
+    public Equipo(String nombre, int golesFavor, int golesContra, int win, int draw, int lost) {
         this.nombre = nombre;
         this.golesFavor = golesFavor;
         this.golesContra = golesContra;
         this.partidos = win + draw + lost;
-        this.puntuacion = win*3 + draw;
+        this.puntuacion = win * 3 + draw;
         this.win = win;
         this.draw = draw;
         this.lost = lost;
+        this.golaverage = golesFavor - golesContra;
     }
-    
-    public Equipo(Equipo e){
+
+    public Equipo(Equipo e) {
         this.nombre = e.nombre;
         this.golesFavor = e.golesFavor;
         this.golesContra = e.golesContra;
         this.partidos = e.win + e.draw + e.lost;
-        this.puntuacion = e.win*3 + e.draw;
+        this.puntuacion = e.win * 3 + e.draw;
         this.win = e.win;
         this.draw = e.draw;
         this.lost = e.lost;
@@ -108,5 +112,74 @@ public class Equipo {
     public void setPartidos(int partidos) {
         this.partidos = partidos;
     }
-    
+
+    public int getGolaverage() {
+        return golaverage;
+    }
+
+    /**
+     * Compara la puntuacion entre dos equipo
+     *
+     * @param o
+     * @return si el primer equipo gana devuelve 1, si pierde -1 y si estan
+     * igualados 0.
+     */
+    @Override
+    public int compareTo(Equipo o) {
+        Integer n = this.getPuntuacion();
+        Integer n2 = o.getPuntuacion();
+        return n.compareTo(n2);
+    }
+
+    public static ArrayList<Equipo> clasificacion(ArrayList<Equipo> array) {
+        ArrayList<Equipo> ret = new ArrayList<>();
+        while (!array.isEmpty()) {
+            if (ret.isEmpty()) {
+                ret.add(array.get(0));
+                array.remove(array.get(0));
+            } else {
+                while (!array.isEmpty()) {
+                    if (ret.get(0).compareTo(array.get(0)) == -1) {
+                        ret.add(0, array.get(0));
+                        array.remove(array.get(0));
+                    } else {
+                        for (int i = 1; i < ret.size(); i++) {
+                            if (ret.get(i).compareTo(array.get(0)) == -1) {
+                                ret.add(i, array.get(0));
+                                array.remove(array.get(0));
+                                break;
+                            } else if (ret.get(i).compareTo(array.get(0)) == 0) {
+                                if (ret.get(i).getGolaverage() > array.get(0).getGolaverage()) {
+                                    ret.add(i + 1, array.get(0));
+                                    array.remove(array.get(0));
+                                    break;
+                                } else if (ret.get(i).getGolaverage() < array.get(0).getGolaverage()) {
+                                    ret.add(i, array.get(0));
+                                    array.remove(array.get(0));
+                                    break;
+                                } else if (ret.get(i).getGolaverage() == array.get(0).getGolaverage()) {
+                                    if (ret.get(i).getNombre().charAt(0) > array.get(0).getNombre().charAt(0)) {
+                                        ret.add(i + 1, array.get(0));
+                                        array.remove(array.get(0));
+                                        break;
+                                    } else {
+                                        ret.add(i, array.get(0));
+                                        array.remove(array.get(0));
+                                        break;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return ret;
+    }
+
+    @Override
+    public String toString() {
+        return this.nombre + " " + this.puntuacion;
+    }
+
 }
--- src/furbol/Furbol.java
+++ src/furbol/Furbol.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package furbol;
+
+import java.util.ArrayList;
+
 public class Furbol {
 
     /**
@@ -14,5 +17,29 @@ public class Furbol {
         Jugador j = new Jugador("Joao", "Felix", 20, 'D', 1);
         j.guardarObjeto();
         Jugador.leerObjeto();
+        
+        Equipo ATM = new Equipo("Atletico de Madrid", 20, 2, 10, 2, 2);
+        Equipo GT = new Equipo("Getafe", 10, 2, 5, 7, 2 );
+        Equipo VAL = new Equipo("Valencia", 16, 6, 2, 2, 2);
+        Equipo RAC = new Equipo("Racing de Santader", 15, 6, 7, 5, 2);
+        Equipo OV = new Equipo("Oviedo", 6, 10, 1, 10, 3);
+        Equipo SPO = new Equipo("Sporting de Gijón", 26, 2, 10, 2, 2);
+        
+        ArrayList<Equipo> clasificacion = new ArrayList<>();
+        clasificacion.add(VAL);
+        clasificacion.add(SPO);
+        clasificacion.add(OV);
+        clasificacion.add(RAC);
+        clasificacion.add(GT);
+        clasificacion.add(ATM);
+        
+        
+        ArrayList<Equipo> ordenada = Equipo.clasificacion(clasificacion);
+       
+        for (Equipo e : ordenada) {
+            System.out.println(e);
+        }
+        int n = 1;
+        
     }
 }
