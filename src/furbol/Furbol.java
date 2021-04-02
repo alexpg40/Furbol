d21bf6c027888ed5b79d53845757b3754bb06bf5
alex <alex@alex>
2021-04-02 16:12:14 +0200

    PRUEBA COMMIT

--- src/furbol/Furbol.java
+++ src/furbol/Furbol.java
@@ -15,7 +15,7 @@ public class Furbol {
      */
     public static void main(String[] args) {
         
-        //PRUEBA COMMT
+        //PRUEBA CsadOMMT
         ArrayList<Equipo> equipos = Utilidades.arraytoArrayList(Utilidades.EQUIPOS);
         ArrayList<Equipo> equiposc = Equipo.clasificacion(equipos);
         Utilidades.leerArrayList(equiposc);
