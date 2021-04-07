61f690cca44774b654683ba8c75c63857a5de08b
alexpg40 <aperezg40@educantabria.es>
2021-04-07 23:57:54 +0200

    añadido metodo gamesOf que devuelve los partidos que juega un equipo

--- src/furbol/Competicion.java
+++ src/furbol/Competicion.java
@@ -17,22 +17,23 @@ public class Competicion {
     
     private String nombre;
     private int temporada;
-    private ArrayList<Equipo> participantes = new ArrayList<>();;
+    private ArrayList<Equipo> participantes = new ArrayList<>();
+    ;
     private LinkedHashSet<Equipo> clasificacion;
     private ArrayList<Partido> partidos = new ArrayList<>();
-
-    public Competicion(){
-        
-    } 
     
-    public Competicion(String nombre,int temporada, ArrayList<Equipo> participantes){
+    public Competicion() {
+        
+    }    
+    
+    public Competicion(String nombre, int temporada, ArrayList<Equipo> participantes) {
         this.nombre = nombre;
         this.temporada = temporada;
         this.participantes = participantes;
         this.clasificacion = new LinkedHashSet<>(participantes);
     }
     
-    public Competicion(Competicion e){
+    public Competicion(Competicion e) {
         this.nombre = e.nombre;
         this.temporada = e.temporada;
         this.clasificacion = e.clasificacion;
@@ -42,51 +43,49 @@ public class Competicion {
     public String getNombre() {
         return nombre;
     }
-
+    
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
+    
     public int getTemporada() {
         return temporada;
     }
-
+    
     public void setTemporada(int temporada) {
         this.temporada = temporada;
     }
-
+    
     public LinkedHashSet<Equipo> getClasificacion() {
         return clasificacion;
     }
-
+    
     public void setClasificacion(LinkedHashSet<Equipo> clasificacion) {
         this.clasificacion = clasificacion;
     }
-
+    
     public ArrayList<Equipo> getParticipantes() {
         return participantes;
     }
-
+    
     public void setParticipantes(ArrayList<Equipo> participantes) {
         this.participantes = participantes;
     }
-
+    
     public ArrayList<Partido> getPartidos() {
         return partidos;
     }
-
+    
     public void setPartidos(ArrayList<Partido> partidos) {
         this.partidos = partidos;
     }
     
-    
-    
-    public Equipo winner(){
+    public Equipo winner() {
         Iterator it = this.getClasificacion().iterator();
         return (Equipo) it.next();
     }
     
-    public Competicion newCompeticion(String nombre, int temporada, ArrayList<Equipo> participantes){
+    public Competicion newCompeticion(String nombre, int temporada, ArrayList<Equipo> participantes) {
         Competicion ret = new Competicion(nombre, temporada, participantes);
         for (Equipo e : participantes) {
             for (Equipo eq : participantes) {
@@ -100,14 +99,29 @@ public class Competicion {
         }
         ret.setParticipantes(participantes);
         
-    return ret;}
+        return ret;
+    }
     
-    public void infoCompeticion(){
+    public void infoCompeticion() {
         System.out.println("El nombre de la competicion es " + this.nombre);
         System.out.println("Participan los siguentes equipos:");
         for (Equipo e : this.participantes) {
             System.out.println(e.getNombre());
         }
+    }
+    
+    public ArrayList<Partido> gamesOf(Equipo e) {
+        System.out.println("Partidos de " + e.getNombre() + "\n");
+        ArrayList<Partido> ret = new ArrayList<>();
+        for (Partido p : this.partidos) {
+            if (p.getIdequipolocal() == e.getId() || p.getIdequipovisitante() == e.getId()) {
+                ret.add(p);
+                }
+        }
+        if (ret.equals(null)) {
+            System.out.println("No tiene partidos");
+        }
+        return ret;
     }
     
 }
