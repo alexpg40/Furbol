8ae751e30ef27118f83c82deb8f59e656a28fc2e
Alex <aperezg40@educantabria.es>
2021-04-17 00:07:16 +0200

    :C

---
+++ src/ConexionDAO/ConexionBD.java
@@ -0,0 +1,58 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ConexionDAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import utilidades.Utilidades;
+
+/**
+ *
+ * @author alex
+ */
+public class ConexionBD {
+    
+    private static final String DB_URL = Utilidades.readXML("url");
+    private static final String USER = Utilidades.readXML("user");
+    private static final String PASS = Utilidades.readXML("password");
+    
+    static Connection conn = null;
+    Statement stmt = null;
+    PreparedStatement pstmt = null;
+    
+    public static Connection establecerConexion() {
+        try {
+            //System.out.println("Conectando a la Base de Datos...");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+        } catch (SQLException ex) {
+            System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Se ha producido una ClassNotFoundException:" + ex.getMessage());
+            Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            return conn;
+        }
+    }
+    public static void cerrarConexion() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(ConexionBD.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+    
+}
---
+++ src/dao/JugadorDAO.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import ConexionDAO.ConexionBD;
+import furbol.Jugador;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author alex
+ */
+public class JugadorDAO {
+
+    private Jugador jugador;
+    private static Connection conn;
+
+    public JugadorDAO() {
+        conn = ConexionBD.establecerConexion();
+    }
+
+    public ArrayList<Jugador> todasParcelas() {
+        ArrayList<Jugador> ret = new ArrayList<Jugador>();
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = ConexionBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("SELECT * FROM Equipo");
+                ResultSet prs = pstmt.executeQuery();
+                while (prs.next()) {
+                    int id = prs.getInt("id");
+                    String nombre = prs.getString("nombre");
+                    String apellido = prs.getString("apellido");
+                    int goles = prs.getInt("privada");
+                    char posicion = prs.getString("posicion").charAt(0);
+                    int idequipo = prs.getInt("idEquipo");
+                    Jugador jugador = new Jugador(id, nombre, apellido, goles, posicion, idequipo);
+                    ret.add(jugador);
+                }
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(JugadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (conn != null) {
+                    ConexionBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JugadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return ret;
+    }
+
+}
