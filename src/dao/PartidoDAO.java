0d3c2e7badbc3260475eeec5d9774abc0c024ec2
Alex <aperezg40@educantabria.es>
2021-04-18 21:35:49 +0200

    metodo guardarPartidos

--- src/dao/PartidoDAO.java
+++ src/dao/PartidoDAO.java
@@ -22,11 +22,11 @@ import java.util.logging.Logger;
  * @author alex
  */
 public class PartidoDAO {
-    
+
     private Partido partido;
     private static Connection conn;
-    
-    public static ArrayList<Partido> todosPartidos(){
+
+    public static ArrayList<Partido> todosPartidos() {
         ArrayList<Partido> ret = new ArrayList<>();
         try {
             if (conn == null || conn.isClosed()) {
@@ -38,10 +38,10 @@ public class PartidoDAO {
                 ResultSet prs = pstmt.executeQuery();
                 while (prs.next()) {
                     int id = prs.getInt("idPartido");
-                    int idequipolocal = prs.getInt("nombre");
-                    int idequipovisitante = prs.getInt("apellido");
-                    int goleslocal = prs.getInt("goleslocal");
-                    int golesvisitante = prs.getInt("golesvisitante");
+                    int idequipolocal = prs.getInt("idEquipoLocal");
+                    int idequipovisitante = prs.getInt("idEquipoVisitante");
+                    int goleslocal = prs.getInt("goles_local");
+                    int golesvisitante = prs.getInt("goles_visitante");
                     Date fecha = prs.getDate("fecha");
                     Partido p = new Partido(id, idequipolocal, idequipovisitante, goleslocal, golesvisitante, fecha);
                     ret.add(p);
@@ -58,6 +58,38 @@ public class PartidoDAO {
         } catch (SQLException ex) {
             Logger.getLogger(JugadorDAO.class.getName()).log(Level.SEVERE, null, ex);
         }
-    return ret;}
+        return ret;
+    }
+
+    public static void guardarPartidos(ArrayList<Partido> partidos) {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = ConexionBD.establecerConexion();
+            }
+            try {
+                PreparedStatement pstmt = null;
+                pstmt = conn.prepareStatement("INSERT INTO Partido (idPartido, idEquipoLocal, idEquipoVisitante, goles_local, goles_visitante) VALUES (?, ?, ?, ?, ?)");
+                for (Partido p : partidos) {
+                    pstmt.setInt(1, p.getId());
+                    pstmt.setInt(2, p.getIdequipolocal());
+                    pstmt.setInt(3, p.getIdequipovisitante());
+                    pstmt.setInt(4, p.getGoleslocal());
+                    pstmt.setInt(5, p.getGolesvisitante());
+                    pstmt.executeLargeUpdate();
+                }
+            } catch (SQLException ex) {
+                System.out.println("Se ha producido una SQLException:" + ex.getMessage());
+                Logger.getLogger(JugadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                if (conn != null) {
+                    ConexionBD.cerrarConexion();
+                }
+            }
+
+        } catch (SQLException ex) {
+            Logger.getLogger(JugadorDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
     
 }
